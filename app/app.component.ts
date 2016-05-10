8d2fb5b0bf78b6bc0079d657b5edc3371d714c98
salfonso <stevenoalfonso@gmail.com>
2016-05-10 00:43:28 -0500

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template:`
+  <h1>{{title}}</h1>
+  <h2>My Heroes</h2>
+<ul class="heroes">
+  <li *ngFor="let hero of heroes">
+    <span class="badge">{{hero.id}}</span> {{hero.name}}
+  </li>
+</ul>
+  <h2>{{hero.name}} details</h2>
+  <div><label>id:&nbsp;</label>{{hero.id}}</div>
+  <div><label>name:&nbsp;</label>{{hero.name}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name" />
+  </div>
+  `
+
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  public heroes = HEROES;
+}
+
+export class Hero {
+  id: number;
+  name: string;  
+}
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
